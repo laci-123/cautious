60b009ef0959aca51a12b76158c58bdb8347033d
Gohér László <goher.laci@gmail.com>
2026-03-07 13:03:21 +0100

    colorful output

--- cautious.sh
+++ cautious.sh
@@ -2,6 +2,12 @@
 
 object_file="$1"
 
+BOLD='\033[1m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RESET='\033[0m'
+
 function print_c_file() {
   echo "#include <stdio.h>"                    >  "$1"
   echo "void $function(void);"                 >> "$1"
@@ -22,17 +28,17 @@ do
   if { ./"$function"; } > "$function.stdout.txt" 2> "$function.stderr.txt"
   then
     pass_count+=1
-    echo "[PASS] $function"
+    echo -e "${BLUE}${BOLD}[${GREEN}${BOLD}PASS${BLUE}${BOLD}] $function${RESET}"
   else
     fail_count+=1
-    echo "[FAIL] $function"
+    echo -e "${BLUE}${BOLD}[${RED}${BOLD}FAIL${BLUE}${BOLD}] $function${RESET}"
     echo "  stdout:"
     cat "$function.stdout.txt" | sed 's/^/    /'
     echo "  stderr:"
-    cat "$function.stderr.txt" | sed 's/^/    /' | sed 's|./"\$function"$||'
+    cat "$function.stderr.txt" | sed 's/^/    /' | sed 's|./"\$function"$||' | grep --color=always '^\|Assertion .* failed'
   fi
 done
 
 echo ""
 echo ""
-echo "passed: $pass_count failed: $fail_count"
+echo -e "${BLUE}${BOLD}passed: ${GREEN}${BOLD}$pass_count ${BLUE}${BOLD}failed: ${RED}${BOLD}$fail_count$RESET"
